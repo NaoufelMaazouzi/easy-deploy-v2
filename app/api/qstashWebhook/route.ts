4e8c26b81b4b2c7f413929e60438aad458e30f31
Naoufel <naoufel_du_28@live.fr>
2024-06-24 16:46:15 +0200

    add published true for all pages generated

--- app/api/qstashWebhook/route.ts
+++ app/api/qstashWebhook/route.ts
@@ -24,6 +24,7 @@ export async function POST(req: Request) {
         .update({
           firstContent: firstGeneratedContent,
           secondContent: secondGeneratedContent,
+          published: true,
         })
         .eq("service", service.toLowerCase())
         .eq("contentGenerated", false);
