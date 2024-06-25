55bfcc7460fbaa71d6c79160c202d3d556519b3b
Naoufel <naoufel_du_28@live.fr>
2024-06-26 00:19:58 +0200

    change layout

--- app/(dashboard)/site/[id]/settings/layout.tsx
+++ app/(dashboard)/site/[id]/settings/layout.tsx
@@ -1,8 +1,9 @@
 import { ReactNode } from "react";
-// import { getSession } from "@/lib/auth";
-// import prisma from "@/lib/prisma";
 import { notFound, redirect } from "next/navigation";
 import SiteSettingsNav from "./nav";
+import { createClient } from "@/utils/supabase/server";
+import { Badge } from "@/components/ui/badge";
+import Link from "next/link";
 
 export default async function SiteAnalyticsLayout({
   params,
@@ -11,40 +12,39 @@ export default async function SiteAnalyticsLayout({
   params: { id: string };
   children: ReactNode;
 }) {
-  // const session = await getSession();
-  // if (!session) {
-  //   redirect("/login");
-  // }
-  // const data = await prisma.site.findUnique({
-  //   where: {
-  //     id: decodeURIComponent(params.id),
-  //   },
-  // });
+  const supabase = createClient();
+  const { data: siteData, error } = await supabase
+    .from("sites_with_users")
+    .select("*")
+    .eq("id", Number(params.id))
+    .single();
+  if (error) {
+    notFound();
+  }
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (!siteData || user?.id !== siteData.user_id) {
+    notFound();
+  }
 
-  // if (!data || data.userId !== session.user.id) {
-  //   notFound();
-  // }
-
-  // const url = `${data.subdomain}.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`;
-
+  const url = `https://${siteData.subdomain ? `${siteData.subdomain}.` : ""}${siteData.customDomain ? `${siteData.customDomain}` : `${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`}`;
   return (
     <>
       <div className="flex flex-col items-center space-x-4 space-y-2 sm:flex-row sm:space-y-0">
-        <h1 className="font-cal text-xl font-bold dark:text-white sm:text-3xl">
-          Settings for
-        </h1>
-        {/* <a
-          href={
-            process.env.NEXT_PUBLIC_VERCEL_ENV
-              ? `https://${url}`
-              : `http://${data.subdomain}.localhost:3000`
-          }
-          target="_blank"
-          rel="noreferrer"
-          className="truncate rounded-md bg-stone-100 px-2 py-1 text-sm font-medium text-stone-600 transition-colors hover:bg-stone-200 dark:bg-stone-800 dark:text-stone-400 dark:hover:bg-stone-700"
-        >
-          {url} ↗
-        </a> */}
+        <div className="flex gap-10">
+          <h1 className="font-cal text-xl font-bold dark:text-white sm:text-3xl">
+            Mon site {siteData.name}
+          </h1>
+          <Link
+            href={url}
+            replace
+            className="truncate rounded-md bg-stone-100 px-2 py-2 text-sm font-medium text-stone-600 transition-colors hover:bg-stone-200 dark:bg-stone-800 dark:text-stone-400 dark:hover:bg-stone-700"
+          >
+            {url} ↗
+          </Link>
+          <Badge>{siteData.numberOfPages} pages</Badge>
+        </div>
       </div>
       <SiteSettingsNav />
       {children}
